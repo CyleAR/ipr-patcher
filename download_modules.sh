4de0dc04809a36afb53404f46561aa26a0617a77
pinisok <prain3ps@gmail.com>
2026-04-03 00:10:54 +0900

    fix

--- download_modules.sh
+++ download_modules.sh
@@ -5,6 +5,7 @@ LOCALIFY_KR_NAME=GakumasLocalify_v3.2.0k.apk
 APKEEP_LINK=https://github.com/EFForg/apkeep/releases/latest/download/apkeep-x86_64-unknown-linux-gnu
 APKEEP_NAME=apkeep
 
+aria2c -j16 $APKEEP_LINK -o $APKEEP_NAME
 chmod +x $APKEEP_NAME
 
 echo "LOCALIFY_KR_NAME=$LOCALIFY_KR_NAME" >> "$GITHUB_ENV"
